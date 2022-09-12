59e5aa46e8a8b3f9bf99998b614be55465d9655f
Hessa54 <hessafahad54@gmail.com>
2022-09-13 01:45:21 +0300

    Capstone-2 ✅

---
+++ FlickerImagesMapFinal /FlickerImagesMapFinal/FlickerCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickerCell.swift
+//  FlickerImagesMapFinal
+//
+//  Created by Raneem Alkahtani on 11/09/2022.
+//
+
+import UIKit
+
+class FlickerCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var ImageCell: UIImageView!
+}
---
+++ FlickerImagesMapFinal /Pods/Target Support Files/Pods-FlickerImagesMapFinalTests/Pods-FlickerImagesMapFinalTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlickerImagesMapFinalTests : NSObject
+@end
+@implementation PodsDummy_Pods_FlickerImagesMapFinalTests
+@end
