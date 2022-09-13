99e363647ae0f0d5cb46e83158537dcecd8ad797
Reof <ree.1d@outlook.sa>
2022-09-13 03:17:06 +0300

    Upload Capstone 2

---
+++ Capstone111/Capstone111/Controller/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Capstone111
+//
+//  Created by Anaal Albeeshi on 15/02/1444 AH.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    // var imageView =
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var ImageView: UIImageView!
+}
